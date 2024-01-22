3fb61b0da1fd01c637e2113e81d008417a36d520
AlirezaIvaz <ivaz1382@gmail.com>
2024-01-22 14:58:04 +0330

    Migrate to view binding

--- app/src/main/java/ir/alirezaivaz/zoomy/sample/MainActivity.kt
+++ app/src/main/java/ir/alirezaivaz/zoomy/sample/MainActivity.kt
@@ -4,15 +4,18 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import ir.alirezaivaz.zoomy.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mainActivity = this@MainActivity
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     private val mImages by lazy {
         arrayListOf(
             R.drawable.img1, R.drawable.img2,
@@ -24,12 +27,13 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        val recyclerView = findViewById<View>(R.id.rv) as RecyclerView
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-        recyclerView.addItemDecoration(RecyclerSpaceDecoration(5))
-        recyclerView.adapter = ImageAdapter(this@MainActivity, mImages)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        with(binding.recyclerView) {
+            layoutManager = GridLayoutManager(mainActivity, 2)
+            addItemDecoration(RecyclerSpaceDecoration(5))
+            adapter = ImageAdapter(mainActivity, mImages)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
